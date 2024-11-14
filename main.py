73947c4179c67bfc612c2ca8622210309c66e78b
Pierre <pierre.vauterin@ensiie.fr>
2024-11-14 18:04:52 +0100

    Correcting card indexes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 def openPack():
     for i in range(nbCardsInPack):
         value = random.randint(1,100)
-        if value==legendaryDrop: print(f"Card #{i} is legendary!")
-        elif value>legendaryDrop and value <=epicDrop: print(f"Card {i} is epic!")
-        elif value>epicDrop and value <=rareDrop: print (f"Card {i} is just rare...")
+        if value==legendaryDrop: print(f"Card #{i+1} is legendary!")
+        elif value>legendaryDrop and value <=epicDrop: print(f"Card {i+1} is epic!")
+        elif value>epicDrop and value <=rareDrop: print (f"Card {i+1} is just rare...")
+        else: print(f"Card {i+1} is common...")
     pass
 
 
