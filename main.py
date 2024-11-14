08ea2ad437e8f5d7a0183f5851547a9f68f8fbde
Pierre <pierre.vauterin@ensiie.fr>
2024-11-14 18:59:53 +0100

    Changed drop rates for the better

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cardCollection import getSpecificCard
 
 legendaryDrop = 1
 epicDrop = 11
-rareDrop = 30
+rareDrop = 34
 dropRates = [rareDrop,epicDrop,legendaryDrop]
 
 nbCardsInPack = 5
@@ -12,7 +12,7 @@ def openPack():
     print("Opening a pack!")
     for i in range(0,nbCardsInPack):
         value = random.randint(1,100)
-        if value==legendaryDrop: print(f"WOW!! Card {i+1} is legendary! You got {getSpecificCard("legendary")}")
+        if value==legendaryDrop: print(f"WOW!! CARD {i+1} IS LEGENDARY! {getSpecificCard("legendary")} HONORS YOU BY ITS PRESENCE")
         elif value>legendaryDrop and value <=epicDrop: print(f"Nice, card {i+1} is epic! You got {getSpecificCard("epic")}")
         elif value>epicDrop and value <=rareDrop: print (f"Oh, card {i+1} is rare... You got {getSpecificCard("rare")}")
         else: print(f"Card {i+1} is common. You got {getSpecificCard("common")}")
