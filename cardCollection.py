be79bd7479aec50132e64f1f9aa5984043ffb302
Pierre <pierre.vauterin@ensiie.fr>
2024-11-14 18:32:44 +0100

    Adding specific cards in packs

--- cardCollection.py
+++ cardCollection.py
@@ -1,4 +1,19 @@
+import random
+
 legendaryCards = ["Gormok the destroyer", "Finizul", "Joe"]
 epicCards = ["Arathula the spider mother", "Nevastros lord of the oceans", "Menezul the demon lord"]
 rareCards = ["Knight", "Greater demon","Hungry shark"]
 commonCards = ["Peasant", "Clown fish", "Spider","Imp"]
+
+cards = [commonCards,rareCards,epicCards,legendaryCards]
+
+def getSpecificCard(rarity,cards=cards):
+    if rarity=="common":
+        return cards[0][random.randint(0,len(cards[0])-1)]
+    elif rarity == "rare":
+        return cards[1][random.randint(0,len(cards[1])-1)]
+    elif rarity == "epic":
+        return cards[2][random.randint(0,len(cards[2])-1)]
+    elif rarity == "legendary":
+        return cards[3][random.randint(0,len(cards[3])-1)]
+    
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random 
+from cardCollection import getSpecificCard
 
 def openPack():
     for i in range(nbCardsInPack):
         value = random.randint(1,100)
-        if value==legendaryDrop: print(f"Card #{i+1} is legendary!")
-        elif value>legendaryDrop and value <=epicDrop: print(f"Card {i+1} is epic!")
-        elif value>epicDrop and value <=rareDrop: print (f"Card {i+1} is just rare...")
-        else: print(f"Card {i+1} is common...")
+        if value==legendaryDrop: print(f"Card {i+1} is legendary! You got {getSpecificCard("legendary")}")
+        elif value>legendaryDrop and value <=epicDrop: print(f"Card {i+1} is epic! You got {getSpecificCard("epic")}")
+        elif value>epicDrop and value <=rareDrop: print (f"Card {i+1} is just rare... You got {getSpecificCard("rare")}")
+        else: print(f"Card {i} is common. You got {getSpecificCard("common")}")
     pass
 
 
