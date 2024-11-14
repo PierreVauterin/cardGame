6db766e91ca215baede17d31dc21ed4cf1a87325
Pierre <pierre.vauterin@ensiie.fr>
2024-11-14 18:25:40 +0100

    Starting the card collection

---
+++ cardCollection.py
@@ -0,0 +1,4 @@
+legendaryCards = ["Gormok the destroyer", "Finizul", "Joe"]
+epicCards = ["Arathula the spider mother", "Nevastros lord of the oceans", "Menezul the demon lord"]
+rareCards = ["Knight", "Greater demon","Hungry shark"]
+commonCards = ["Peasant", "Clown fish", "Spider","Imp"]
