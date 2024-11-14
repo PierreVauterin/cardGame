7bcde30fe81fba69250717d197fb76a96180779b
Pierre <pierre.vauterin@ensiie.fr>
2024-11-14 18:48:48 +0100

    Added droprates and better printing

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import random 
 from cardCollection import getSpecificCard
 
+legendaryDrop = 1
+epicDrop = 11
+rareDrop = 30
+dropRates = [rareDrop,epicDrop,legendaryDrop]
+
+nbCardsInPack = 5
+
 def openPack():
+    print("Opening a pack!")
     for i in range(nbCardsInPack):
         value = random.randint(1,100)
         if value==legendaryDrop: print(f"Card {i+1} is legendary! You got {getSpecificCard("legendary")}")
         elif value>legendaryDrop and value <=epicDrop: print(f"Card {i+1} is epic! You got {getSpecificCard("epic")}")
         elif value>epicDrop and value <=rareDrop: print (f"Card {i+1} is just rare... You got {getSpecificCard("rare")}")
         else: print(f"Card {i} is common. You got {getSpecificCard("common")}")
-    pass
+    print("\n")
 
+def printDropRates(dropRates=dropRates):
+    print(f"Common card: {100-dropRates[0]}%\nRare card: {dropRates[0]-dropRates[1]}%\nEpic card: {dropRates[1]-dropRates[2]}%\nLegendary card: {dropRates[2]}%\n")
 
-epicDrop = 15
-rareDrop = 30
-legendaryDrop = 1
-nbCardsInPack = 5
+printDropRates()
 
+openPack()
 openPack()
