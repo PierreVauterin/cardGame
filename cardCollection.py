a25e545a45c14b7932b8692123ad49bcdd325d3a
Pierre <pierre.vauterin@ensiie.fr>
2024-11-15 18:04:07 +0100

    Added some new cards to pack

--- cardCollection.py
+++ cardCollection.py
@@ -1,16 +1,16 @@
 import random
 
 secretCards = ["The treasure"]
-legendaryCards = ["Gormok the destroyer", "Finizul", "Joe"]
-epicCards = ["Arathula the spider mother", "Nevastros lord of the oceans", "Menezul the demon lord"]
-rareCards = ["Knight", "Greater demon","Hungry shark"]
-commonCards = ["Peasant", "Clown fish", "Spider","Imp"]
+legendaryCards = ["Gormok the destroyer", "Finizul", "Joe", "The end of times", "The Everdream", "Herodos, the lonely planet"]
+epicCards = ["Arathula the spider mother", "Nevastros lord of the oceans", "Menezul the demon lord", "Herodos the lonely planet","Skibidisigma the Brainrot"]
+rareCards = ["Knight", "Greater demon","Hungry shark", "Fireball","Drain life","Shaboinky"]
+commonCards = ["Peasant", "Clown fish", "Spider","Imp","Drop of water","Match"]
 
 cards = [commonCards,rareCards,epicCards,legendaryCards,secretCards]
 
 def getSpecificCard(rarity,cards=cards):
     if rarity=="common":
-        return cards[0][random.randint(0,len(cards[0])-1)]
+        return cards[0][random.randint(0,len(cards[0])-1)] 
     elif rarity == "rare":
         return cards[1][random.randint(0,len(cards[1])-1)]
     elif rarity == "epic":
